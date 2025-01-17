5b4eb83121fc48a26d40745817f715d5a158df4d
Aditya yadav <adityayadavadi6100@gmail.com>
2025-01-17 14:38:14 +0530

    new codes

---
+++ JAN.17/literals.py
@@ -0,0 +1,39 @@
+x=5+3j
+print(type(x))
+y=10
+print(type(y))
+z=10.2
+print(type(z))
+
+d='x'
+print(type(d))
+g='''a'''
+print(type(g))
+A = '''
+       (..)
+       /  \ 
+        ||'''
+print(A)
+
+# pep8
+
+t=True
+print(type(t))
+t1=False
+print(type(t1))
+
+my_list=[10,20,30,40,'neeraj',10.2,10+2j,[1,2,3,4]]
+print(type(my_list))
+my_taple=(10,20,30,40,'neeraj',10.2,10+2j,[1,2,3,4])
+print(my_taple)
+print(type(my_taple))
+
+my_dict={'name':'neeraj','age':22,'quali':'m.tach'}
+print(my_dict)
+print(type(my_dict))
+my_set={10,20,30,'neeraj','rahul',10,20,30,10.5,'rahul'}
+print(my_set)
+print(type(my_set))
+my_frozenset=frozenset({10,20,30,'neeraj','rahul',10,20,30,10.5,'rahul'})
+print(my_frozenset)
+print(type(my_frozenset))
