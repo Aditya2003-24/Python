0ac1f502bb4de239b35a9e31ccb074487035a1eb
Aditya yadav <adityayadavadi6100@gmail.com>
2025-01-30 14:31:26 +0530

    new code

---
+++ jan.30/control_statement.py
@@ -0,0 +1,23 @@
+#union and bainary
+
+#conditional statement
+
+z=int(input("enter any number: "))
+if (z%2==0):
+    print(" This is Even number")
+else:
+    print("This is Odd number")
+
+x=int(input("enter first number: "))
+y=int(input("enter second number: "))
+
+if x>y:
+    print('x is greater')
+else:
+    print(f'{y} is greater')
+
+q=int(input("enter your age: "))
+if (q>=18):
+    print('your  elegible for vote')
+else:
+    print('your not elegible for vote')
