f811ed35af6080b04e35be1564761f9d6bcfad66
Aditya yadav <adityayadavadi6100@gmail.com>
2025-02-05 14:22:00 +0530

    new code

---
+++ jan.05/_forloop.py
@@ -0,0 +1,16 @@
+# n=input("enter your string")
+# for i in n:
+#     x=ord(i)+4
+#     y=chr(x)
+#     print(y,end=' ')
+
+# n=input("enter your string")
+# if n==n[::-1]:
+    # print('this is palendrom')
+
+
+#range(start,stop,darection)
+
+x=int(input("enter your string"))
+y=range(1,x+1,2)
+print(list(y))
