ce1aa73875fecefc21b5771f8baf8e79771d95d5
Aditya yadav <adityayadavadi6100@gmail.com>
2025-02-14 14:09:31 +0530

    new code

---
+++ feb.14/scop.py
@@ -0,0 +1,46 @@
+#scope of variable => there is two types of scops. 
+#1. local scop
+#2. global scop
+
+# x=10                   #global variable
+# def add():
+#     y=20                #local variable
+#     print(x)
+#     print(y)
+
+# add()
+# print(x)
+# print(y)  Error
+
+# x=10                   #global variable
+# def add():
+#     global y             # this is use for change the scop
+#     y=20                #local variable
+#     print(x)
+#     print(y)
+
+# add()
+# print(x)
+# print(y)
+
+# x=10                   #global variable
+# def add():
+#     global x  
+#     print(x)          # this is use for change the scop
+#     x=20                #local variable
+#     print(x)
+
+# add()
+# print(x)
+
+x=10                   #global variable
+def add():
+    x=20                #local variable
+    print('value of global variable x:',globals()['x'])
+    print(x)
+
+add()
+print(x)
+
+#global is keyword
+#globals is a methoed
