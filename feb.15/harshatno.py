dafbfc792e239b9d4c78ad4ef00eda44cbdcfc41
Aditya yadav <adityayadavadi6100@gmail.com>
2025-02-15 14:52:16 +0530

    new code

---
+++ feb.15/harshatno.py
@@ -0,0 +1,20 @@
+# n=eval(input('enter the no.: '))
+# sum=0
+# x=n
+# while n>0:
+#     last_digit=n%10
+#     sum = sum+last_digit
+#     n=n//10
+# print(sum)
+# print(n)
+# if x%sum==0:
+#     print(f'this is harshat no. {x}')
+# else:
+#     (f'this is not harshat no. {x}')
+
+
+#sorting
+
+n=eval(input('enter the no.: '))
+
+print(n.sorted())
