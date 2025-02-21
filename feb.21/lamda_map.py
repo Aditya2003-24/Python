b84b428921ecc5ac1299e8c4c5fcaf9cf6d30424
Aditya yadav <adityayadavadi6100@gmail.com>
2025-02-21 21:33:01 +0530

    new code

---
+++ feb.21/lamda_map.py
@@ -0,0 +1,35 @@
+# l1=[1,2,3,4,5]
+# res=list(map(lambda x: 'even' if x%2==0 else 'odd',l1))
+# print(res)
+
+# l1=[1,2,3,4,5]
+# res=list(map(lambda x:x**2,l1))
+# print(res)
+
+# l1=[1,2,3,4,5]
+# l2=[1,2,3,4,5]
+
+# res=list(map(lambda x,y:x+y,l1,l2))
+# print(res)
+
+# l1=[1,2,3,4,5]
+# res=list(filter(lambda x:'even' if x%2==0 else '',l1))
+# print(res)
+
+# l1=['neeraj','raj','jay']
+# res=list(map(lambda x: x.upper(),l1))
+# print(res)
+
+# import functools
+
+# l1=[1,2,3,4,5]
+# res=functools.reduce(lambda x,y:x+y,l1)
+
+# print(res)
+l1=5
+for i in range(1,5):
+    print(i.append([1,2,3,4]))
+# res=[j for j range(5) for i in range(5)]
+for _ in range(1,5):
+    print('hi')
+# print(res)
